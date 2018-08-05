c2dcd48069dc916fa474d13d7caaea7567fe6981
zhangdd <2643555865@qq.com>
2018-08-05 16:24:01 +0800

    delete

--- effect_java/src/main/java/适配器模式/德国接口/DBSocket.java
+++
@@ -1,8 +0,0 @@
-package 适配器模式.德国接口;
-
-public class DBSocket implements DBSocketInterface {
-    @Override
-    public void powerWithTwoRound() {
-        System.out.println("使用两项圆头的插孔供电");
-    }
-}
--- effect_java/src/main/java/适配器模式/德国接口/DBTest.java
+++
@@ -1,9 +0,0 @@
-package 适配器模式.德国接口;
-
-public class DBTest {
-    public static void main(String[] args) {
-        DBSocketInterface dbSocket = new DBSocket();
-        Hotel hotel = new Hotel(dbSocket);
-        hotel.charge();
-    }
-}
